891d751df444fb2d690e06380d41ab74a285c740
Tim Acker <tjacker31@gmail.com>
2017-08-19 21:48:11 -0400

    Index
    Split burgers into not eaten and eaten lists. Added button to 'devour' burgers that have not been eaten and a button to delete burgers that were eaten. AJAX methods to follow.

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,3 +2,5 @@ INSERT INTO burgers (burger_name, devoured) VALUES ("Kobe Burger", false);
 INSERT INTO burgers (burger_name, devoured) VALUES ("Ringer", false);
 INSERT INTO burgers (burger_name, devoured) VALUES ("Blanco Burger", false);
 INSERT INTO burgers (burger_name, devoured) VALUES ("Turkey Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Breakfast Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Black Bean Burger", true);
